f0fb9781fcd55655bf064175a7f7900082ca46fe
Md Mustafizur Rahman Sayem <40750318+rahmancoder@users.noreply.github.com>
2026-06-17 06:46:41 +0000

    working with prisma orm

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Prism ORM
+
+It's can be comapare as a translator between Typescript or Javascript to Database language.
+As JS/TS talks a different language than database language , Sometimes rather writing the raw
+database SQL query rather we can use a translator like Prisma, which is known as Object Relational Mapping (ORM) to translate automatically for us. Like: Tables as Schema, Migrate those schema to database , then as a client do the query task like: Create, Read, Update, Delete. (CRUD). So Client will generate the Query from the CLient and will get response for that. Its becomes as easy as translator can help why not use the translator to do the task right?!
+
+# Prisma Setup (Follow the documentation step by step)
--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,7 @@
 import { prisma } from "./lib/prisma";
 
-async function main() {
+async function main() 
+{
     // Create a new user with a post
     // const user = await prisma.user.create({
     //     data : {
@@ -18,32 +19,54 @@ async function main() {
     //         posts : true,
     //     }
     // })
-    const newPost = await prisma.post.create({
-        data: {
+
+
+
+    const newPost = await prisma.post.create(
+        {
+
+        data: 
+        {
             authorId : 1,
             title: "Hello World 2",
             content: "This is my second post as 1st user!",
             published: true,
         },
-    });
+
+       });
+
+
+
     console.log("Created post:", newPost);
     // console.log("Created user:", user);
 
+
     // Fetch all users with their posts
-    const allUsers = await prisma.user.findMany({
+    const allUsers = await prisma.user.findMany(
+        {
         include: {
             posts: true,
         },
+
     });
+
     console.log("All users:", JSON.stringify(allUsers, null, 2));
 }
 
 main()
-    .then(async () => {
+
+
+    .then(async () => 
+        {
         await prisma.$disconnect();
-    })
-    .catch(async (e) => {
+        })
+
+
+    .catch(async (e) => 
+        {
         console.error(e);
+
         await prisma.$disconnect();
+
         process.exit(1);
-    });
+        });
