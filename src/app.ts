55898cda1e7a19a9abad40b5975dcdc9fff852c3
Eh-haque <ehtisamul@gmail.com>
2026-07-02 23:08:41 +0600

    auth and service complete

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import config from "./config";
 import { notFoundHandler } from "./middlewares/not-found";
 import { globalErrorHandler } from "./middlewares/global-error";
+import authRoutes from "./modules/auth/auth.routes";
+import serviceRoutes from "./modules/service/service.routes";
 
 const app: Application = express();
 
@@ -22,6 +25,10 @@ app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
+
+// 1. All your actual API Routes go here
+app.use("/api/auth", authRoutes);
+app.use("/api/service", serviceRoutes);
 
 // 2. ⚠️ THE NOT FOUND MIDDLEWARE (Catches anything that didn't match above)
 app.use(notFoundHandler);
